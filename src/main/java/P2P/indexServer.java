026c47e1e0c0cb868e95ca1290a53a54712367d3
Guangqi Sun <sun357@purdue.edu>
2020-02-10 14:15:57 -0600

    Add indexServer.java

---
+++ src/main/java/P2P/indexServer.java
@@ -0,0 +1,11 @@
+package P2P;
+
+public class indexServer {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
